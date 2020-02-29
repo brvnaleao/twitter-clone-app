99014bce663bab1817e44fc4b6e593f616598f83
Bruna Leão <brunaleao@id.uff.br>
2020-02-29 15:09:35 -0300

    added db connection + loading data on the ui

--- server/server.js
+++ server/server.js
@@ -1,20 +1,50 @@
 const express = require('express')
 const cors = require('cors')
+const monk = require('monk')
+
+const db = monk('localhost/meower')
+
+const mews = db.get('mews')
+
 
 var bodyParser = require('body-parser');
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) =>{
-    res.send("dasd")
+app.get('/mews', (req, res) =>{
+    mews.find().then(mews => {
+        res.status(200).json(mews)
+    })
     
 })
-app.post('/test', (req, res)=>{
-    console.log(req.body);
-    res.status(200).send(req.body)
+
+function isValidMew(mew){
+    return mew.name && mew.name.toString().trim() !== '' &&
+    mew.content && mew.content.toString().trim() !== '';
+}
+
+app.post('/mews', (req, res)=>{
+    if(isValidMew(req.body)){
+        const mew = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString(),
+            created: new Date()
+        }
+
+
+        mews.insert(mew).then(createdMew =>{
+            res.status(200).json(createdMew)
+        })
+
+    }else{
+        res.status(422);
+        res.json({
+            message: "Hey! Name and Content are required" 
+        })
+    }
 })
 
 app.listen(1231, () =>{
-    console.log('linstening no http://localhost:1231')
+    console.log('linstening on http://localhost:1231')
 })
--- ui/scripts/script.js
+++ ui/scripts/script.js
@@ -1,9 +1,12 @@
-console.log("hi")
-
 const form = document.getElementById('form');
 const loadingElement = document.querySelector('.loading');
-const API_URL = 'http://localhost:1231/test'
-loadingElement.style.display = 'none';
+const mewsElement = document.querySelector('.mews');
+const API_URL = 'http://localhost:1231/mews'
+
+loadingElement.style.display = '';
+
+listAllMews()
+
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
     const formData = new FormData(form);
@@ -14,7 +17,10 @@ form.addEventListener('submit', (e) =>{
         name,
         content
     }
-    console.log(mew)
+    loadingElement.style.display = '';
+    form.style.display = 'none';
+    
+   
     fetch(API_URL, {
         method: "POST",
         body: JSON.stringify(mew),
@@ -22,8 +28,42 @@ form.addEventListener('submit', (e) =>{
             'content-type': 'application/json'
         }
     })
-    loadingElement.style.display = '';
-    form.style.display = 'none';
+        .then(res => res.json())
+        .then(createdMew => {
 
-
+            console.log(createdMew)
+            form.reset()
+            loadingElement.style.display = 'none'
+            form.style.display = ''
+    })
 })
+
+function listAllMews(){
+
+    fetch(API_URL,{
+        method: "GET",
+    }).then(res => res.json()).then(mews => {
+
+        loadingElement.style.display = 'none';
+        mews.forEach(mew => {
+            console.log(mews)
+            const div = document.createElement('div')
+            const header = document.createElement('h3')
+            header.textContent = mew.name
+            const date = document.createElement('h6')
+            date.textContent = mew.created
+
+            const content = document.createElement('p')
+            content.textContent = mew.content
+            div.appendChild(header)
+            div.appendChild(content)
+            div.appendChild(date)
+            mewsElement.appendChild(div)
+
+        
+        });
+        
+        
+    
+    })
+}
